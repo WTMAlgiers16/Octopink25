7b3af0c9b727f0a11c8f20152e4809fba8ac1dd2
kawthar <kawtharikram2020@gmail.com>
2025-09-13 10:48:25 +0100

    chore(page): update homepage to include ObjectivesSection

--- octopink25/src/app/page.tsx
+++ octopink25/src/app/page.tsx
@@ -3,10 +3,7 @@ import { ObjectivesSection } from "../components/objectives-section";
 export default function Home() {
   return (
     <main>
-      {/* Other sections of your homepage... */}
       <ObjectivesSection />{" "}
-      {/* Add this line where you want the section to appear */}
-      {/* Other sections of your homepage... */}
     </main>
   );
 }
