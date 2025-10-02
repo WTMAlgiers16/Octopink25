75f9d13fccfe22fd2f1460feda8936b42f1e1ce7
Rofieda <mr_maamar@esi.dz>
2025-10-02 05:59:54 +0500

    feat: complete Awareness section implementation

--- octopink25/src/app/page.tsx
+++ octopink25/src/app/page.tsx
@@ -4,16 +4,20 @@ import { AIDetectionSection } from "@/components/breast-cancer-section";
 import EventAgenda  from "../components/event-agenda"
 import { ObjectivesSection } from "../components/objectives-section";
 import OrganizersSection from '@/components/organizers';
+import AwarenessSection from "@/components/awareness-section";
 
 export default function HomePage() {
   return (
     <main className="max-w-7xl mx-auto">
       {/* Other components */}
       <ObjectivesSection />
+      <AwarenessSection/>
+      <AIDetectionSection />
       <OrganizersSection />
       <SponsorsSection />
-      <AIDetectionSection />
+      
       <EventAgenda />
+      
       {/* Other components */}
     </main>
   );
